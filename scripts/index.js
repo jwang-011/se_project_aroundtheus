eaa5ed7723a5ad51229d64d3da75edbcc53ea5f6
jwang11.alt <jackwang6453@gmail.com>
2023-12-15 13:55:10 -0500

    Stage 4 Part 4

--- scripts/index.js
+++ scripts/index.js
@@ -44,11 +44,56 @@ const profileEditModal = document.querySelector("#js-profile-edit-modal");
 const profileEditCloseBtn = document.querySelector(
   "#js-edit-modal-close-button"
 );
+const profileEditSaveBtn = document.querySelector("#js-profile-edit-save");
+const profileTitle = document.querySelector("#js-profile-title");
+const profileSubtitle = document.querySelector("#js-profile-subtitle");
+const profileTitleInput = document.querySelector("#js-profile-title-input");
+const profileSubtitleInput = document.querySelector(
+  "#js-profile-subtitle-input"
+);
+
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#js-card-template").content.firstElementChild;
+
+// Functions //
+
+function closePopup() {
+  profileEditModal.classList.toggle("modal__opened");
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardImageEl.setAttribute("src", data.link);
+  cardImageEl.setAttribute("alt", data.name);
+  cardTitleEl.textContent = data.name;
+  return cardElement;
+}
+
+// Event Handlers //
+
+function handleProfileSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileSubtitle.textContent = profileSubtitleInput.value;
+}
+
+// Event Listeners //
 
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.innerText;
+  profileSubtitleInput.value = profileSubtitle.textContent;
   profileEditModal.classList.toggle("modal__opened");
 });
 
-profileEditCloseBtn.addEventListener("click", () => {
-  profileEditModal.classList.toggle("modal__opened");
+profileEditCloseBtn.addEventListener("click", closePopup);
+
+profileEditSaveBtn.addEventListener("click", handleProfileSubmit);
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardListEl.prepend(cardElement);
 });
